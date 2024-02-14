520d668faa3a4d9cd05688a61cf3263c3178c6f9
rrahulraj2003 <rrahulraj2003@gmail.com>
2024-02-13 22:34:43 -0500

    chunk to heap to chunk testing works

--- P1/mymalloc.c
+++ P1/mymalloc.c
@@ -1,19 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "mymalloc.h"
-
+#include <string.h>
 
 #define MEMLENGTH 512
 static double memory[MEMLENGTH];
-
+#define heap ((char *) memory)
 
 void *mymalloc(size_t n, char *file, int line) {
-    void *ptr;
-    chunk new_chunk = {n, 1, ptr};
+    chunk chunky = {7, 444};
+    printf("%ld\n", sizeof(chunk));
+    for(int i = 0; i < sizeof(chunk); i++){
+        heap[i] = ((char *) &chunky)[i];
+    }
 
-    char *heapstart = (char *) memory;
+    chunk chunkyboi;
+
+    for(int i = 0; i < sizeof(chunk); i++){
+        ((char *) &chunkyboi)[i] = heap[i];
+    }
+    printf("data: %d, %d\n", chunkyboi.size, chunkyboi.alloc_stat);
     
+}
 
+int main(){
+    malloc(5);
+    return 0;
 }
 
 void myfree(void *ptr, char *file, int line) {
